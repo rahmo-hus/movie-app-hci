04928141f052b9258b87465176308af98ec96139
Rahmo Huseinagic <45545657+rahmohuseinagic@users.noreply.github.com>
2021-10-06 18:09:32 +0200

    Added multiselect combo box

--- Filmoteka/View/AddMovieView.xaml.cs
+++ Filmoteka/View/AddMovieView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Filmoteka.Model;
+using Filmoteka.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,11 @@ namespace Filmoteka.View
         public AddMovieView()
         {
             InitializeComponent();
+            List<string> countries = MovieDAO.GetGountries(), languages = MovieDAO.GetLanguages();
+            List<Genre> genres = GenreDAO.GetGenres();
+            countries.ForEach(country => comboCountrySelect.Items.Add(country));
+            languages.ForEach(language => comboLanguageSelect.Items.Add(language));
+            genres.ForEach(genre => comboGenreSelect.Items.Add(genre.ToString()));
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
